da88e616cfcf67569eed69e185c3ad4db631de20
eugabrielbr <gabrielnetbrine@gmail.com>
2025-11-21 18:25:22 -0300

    chore: ajustes para deploy

--- frontend/agendamentoApp/src/components/services/api.ts
+++ frontend/agendamentoApp/src/components/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 👉 Troque pela URL REAL da sua API
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 8000,
   headers: {
     "Content-Type": "application/json",
